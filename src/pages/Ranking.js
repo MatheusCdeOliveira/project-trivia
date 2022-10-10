c10e49a07b5afbfa89e81d3a1ee55288ca5a4836
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-10-10 16:27:12 -0300

    requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Ranking extends Component {
+  home = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <>
         <Header />
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.home }
+        >
+          Inicio
+
+        </button>
       </>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default connect()(Ranking);
